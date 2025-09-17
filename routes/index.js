4d1c90627979b3606eb6198c4f0b8d8aba1365ee
KrishnaPeaks <krishna.a3567@gmail.com>
2025-09-17 13:51:49 +0530

    Setup Routers Main

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,33 @@
+const express = require("express");
+const app = express();
+app.post("/user/signup",function(req,res){
+    res.json({
+        msg:"function"
+    })
+})
+
+app.post("/user/signin",function(req,res){
+    res.json({
+        msg:"function"
+    })
+})
+
+app.get("/purchases",function(req,res){
+    res.json({
+        msg:"function"
+    })
+})
+
+app.post("/course/purchase",function(req,res){
+    res.json({
+        msg:"function"
+    })
+})
 
 
-int main 
+app.get("/courses",function(req,res){
+    res.json({
+        msg:"function"
+    })
+})
+
